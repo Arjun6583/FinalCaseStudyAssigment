cdfb02b77278b2867d44249087ce527e6e5e2b26
Arjun <arjunghule1113@gmail.com>
2023-11-09 11:10:08 +0530

    Project is Done

--- home.js
+++ home.js
@@ -1,6 +1,21 @@
 function toggleDropdown(id) {
   const dropdown = document.getElementById(id);
   if (dropdown.style.display === 'none') {
+    if((document.getElementById('radixSortDropdown').style.display)!=='none')
+    {
+      document.getElementById('radixSortDropdown').style.display = 'none';
+    }
+      
+    if( (document.getElementById('bucketSortDropdown').style.display)!=='none')
+    {
+      document.getElementById('bucketSortDropdown').style.display = 'none';
+    }
+      
+    if((document.getElementById('countingSortDropdown').style.display)!=='none')
+    {
+      document.getElementById('countingSortDropdown').style.display='none';
+    }
+
     dropdown.style.display = 'block';
   } else {
     dropdown.style.display = 'none';
